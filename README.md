e71a107da6c9a365501243f410b10e2223289ca5
angeljsus <angeljaguilar96@gmail.com>
2023-02-28 15:57:39 -0600

    Se agrega componente de login con consulta a script de php

--- README.md
+++ README.md
@@ -5,6 +5,42 @@ Repositorio de componentes para ser utilizados dentro de aplicaciones de React.
 
 ### Componentes (./component)
 
+#### `Login`
+
+**Descripción**
+
+El componente permite controlar el acceso a las aplicaciones. Permite autenticar por medio del LDAP y un script de php para comprobar que el usuario se encuentre registrado.
+
+**{ Propiedades }**
+- **urlPeticion** *(string)** : ruta del script donde consulta la existencia de datos.
+- **propiedadBajar** *(string)** : Propiedad del JSON que retorna si es autenticado exitosamente.
+
+
+**Resultados**
+```js
+
+// la solicitud de la aplicación manda los parametros con el nombre de la cuenta 
+// ejemplo: ?cuenta=angel.trujillo
+<Login 
+  propiedadBajar="nombre_usuario" 
+  urlPeticion="http://xxxxxxx/server/users.php"
+/>
+// respuesta auntenticado con LDAP y servicio (consola)
+{
+  autenticado: true
+  message: "Autenticado desde el servicio"
+  response: "Angel Aguilar"
+  side: "Servicio"
+}
+
+// respuesta para usuario
+Usuario y/o contraseña erronea
+Usted NO cuenta con conexión a Internet
+No se encuentra conectado a la red del INEGI
+No se encuentra registrado dentro del servicio
+
+```
+
 #### `ImageLoader`
 **Descripción**
 
@@ -49,6 +85,99 @@ import { select, getDatabase }  from '../logic/querys'; // getDatabase es como a
 <ListaDesplegable megaproyecto={ 2 } />
 ```
 
+#### `EnvioInformacion`
+
+**Descripción**
+
+El componente transfiere la información de las tablas de la base de datos del cliente para almacenarla dentro de la base de datos del servidor.
+
+**{ Propiedades }**
+
+- **usuario** *(integer)** : valor del usuario del que se enviará información.
+- **proyecto** *(integer)** : valor del proyecto que se enviará la información.
+
+**{ Requerimientos }**
+
+- El componente utiliza los estilos desde el archivo `./component/Envi.oInformacion.css`.
+- El componente utiliza las funciones del módulo de consultas `./logic/querys`.
+- Agregar el archivo y nueva ruta al código PHP para recibir la información desde el cliente desde la propiedad del componente `propsExtensible.serverRequest`.
+- El arreglo con los nombres de las tablas de donde se extraerá la información del cliente tomando en cuenta los nombres de las columnas `usuario` y `proyecto`.
+- Tener instalados los siguientes módulos:
+```bash
+npm install --save fs-extra os path archiver-zip-encryptable archiver
+```
+```bash
+
+npm install --save-dev @types/archiver @types/node
+
+```
+
+**Configuración adicional**
+
+- Existe un problema para ejecutar los métodos del módulo `archiver` después del renderizado al exponer el módulo globalmente, para solucionar se creo la función en el archivo de precarga de la aplicación Electron y se expuso de forma global.
+
+```js
+// ./electron/preload.ts
+import archiver from 'archiver';
+import os from 'os';
+import path from 'path';
+import fs from 'fs-extra';
+
+archiver.registerFormat('zip-encryptable', require('archiver-zip-encryptable')); // registrar formato de archiver
+
+const apiArchiver = {
+  createEncriptedZip: (outputzip: Buffer, filelocation: string, password: any) => { 
+    return new Promise( (resolve, reject) => {
+      const stream : NodeJS.WritableStream = fs.createWriteStream(outputzip);
+      resolve(stream)
+      stream.on('error', err => reject(err) );
+    })
+    .then( stream => {
+      const readable : any  = stream;
+      return new Promise( (resolve, reject) => {
+        let nameFile = path.basename(filelocation)
+        const zip = archiver('zip-encryptable',
+          {
+            zlib: { level: 9 },
+            forceLocalTime: true,
+            password : password
+          }
+        )
+        // aca resuelve el stream
+        readable.on('close', () => resolve(outputzip) );
+        zip.pipe(readable);
+        zip.file(filelocation, { name: nameFile });
+        zip.finalize();
+        zip.on('end', () => {/*termina de agregar los archivos*/});
+        zip.on('error', (err) => reject(err) );
+      })
+    })
+  }
+}
+
+contextBridge.exposeInMainWorld('apiArchiver', apiArchiver); // exponer la función para crear el zip
+contextBridge.exposeInMainWorld('os', os);
+contextBridge.exposeInMainWorld('path', path);
+contextBridge.exposeInMainWorld('fs', fs);
+``` 
+- Aún existe un problema para compilar el código y ejecutar el código debido a que archiver no reconoce el tipado de algunos parámetros, para esto se debe  agregar las siguientes lineas en el `@types/arvhiver`
+
+```ts
+declare namespace archiver {
+  
+  type Format =  'zip-encriptable' | 'zip' | 'tar';  // agregar 'zip-encriptable' al formato
+  // ...
+
+  interface ZipOptions {
+    password?: string | undefined; // agregar el campo de contraseña a la interfaz del ZipOptions
+    // ...
+  }
+}
+```
+
+
+
+
 # Funciones React
 
 ### Descripción
@@ -62,11 +191,13 @@ Funciones desarrolladas para ser utilizados dentro de aplicaciones de React.
 *Archivo: ValidarUsuario.tsx*
 
 **Requerimientos**
-- Requiere instalación previa de `react-xml-parser` y el uso de script
-[querys](https://github.com/angeljsus/querys.git).
+
+- Requiere instalación previa de `react-xml-parser`.
+- El componente utiliza las funciones del módulo de consultas `./logic/querys`.
+- Requiere el acceso a la ruta del archivo `./recursos/servidor/consultas.php` en servidor.
 
 ```bash
-npm install react-xml-parser
+npm install --save react-xml-parser
 ```
 ```js
 import { select } from './querys'
@@ -98,7 +229,7 @@ validarUsuario(userName, password)
 })
 .catch( err => {
   console.error(err) 
-  /* objeto json:
+  /* ejemplo errores:
     - { error: 'LocalDB', message: 'could not prepare statement (1 near "*": syntax error)'}
     - { error: 'Server', message: 'No database selected'}
   */
---
+++ component/Login/Login.tsx
@@ -0,0 +1,157 @@
+import { useEffect, useState, useContext} from 'react';
+import { ContextAreaDeTrabajo } from './../../context/ContextAreaDeTrabajo';
+import AppBar from '../AppBar/AppBar';
+import XMLParser from 'react-xml-parser';
+import logoInegi from '../../logos/inegi.png'
+import './Login.css';
+
+const Login = ({ urlPeticion, propiedadBajar }) => {
+
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
+  const [message, setMessage] = useState('');
+
+  const {setModulo, usrApp, setUsrApp } = useContext(ContextAreaDeTrabajo);
+
+  const validaDatos = e => {
+    e.preventDefault()
+    const inptName = user.trim().toLowerCase().split('@');
+    const userName = inptName[0];
+    const passWord = pass.trim();
+    const dominio = inptName[1];
+    if (userName === 'admin' && passWord === 'admin') {
+      // console.log('Login ok - Es damin');
+    }
+    else if (!userName  || !passWord  ) {
+      console.log('Login off - No se ingreso ningun dato');
+      setMessage('No se ingreso ningun dato')
+    }
+    else if( userName  && passWord  ) {
+      const ldapSite = 'https://intranet.wapp2.inegi.gob.mx/sistemas/informaticos/ws/v2/ldap.asmx';
+      const claveApp = 'DGES_NodeJs_GenContCap';
+      const connection = navigator.onLine;
+      if(!dominio || dominio === 'inegi.org.mx'){
+        const authPromise = connection ? 
+        obtenerAutenticacion(userName, passWord, ldapSite, claveApp) : 
+        Promise.resolve({message:`Usted NO cuenta con conexión a Internet`});
+
+        return authPromise.then( result => {
+          if(result.autenticado){
+            // result response es el parametro obtenido 
+            console.log('Que hacer aquí? Acceso concedido: ', result.autenticado)
+            console.log(result)
+            // setUsrApp(result.idusuario)
+            // agregar modulo de inicio
+            // setModulo('HistoriasUsuario')
+          }
+          setMessage(result.message)
+        })
+      }
+      return setMessage(`El dominio @${dominio} no pertenece a el INEGI`); 
+    } 
+  }
+
+  const obtenerAutenticacion = (userName, passWord, ldapSite, claveApp) => {
+    const params = `loginUsr=${userName}&passUsr=${encodeURIComponent(passWord)}&claveAplicacion=${claveApp}`;
+    const urlAuth = new URL(`${ldapSite}/Autenticar?${params}`, ldapSite);
+    return new Promise( (resolve, reject) => {
+      return fetch(urlAuth,{
+        method: 'GET',
+        headers : {
+         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8;',
+        }
+       })
+      .then( response => response.status === 404 ? Promise.reject() : response.text())
+      .then( text => new XMLParser().parseFromString(text, 'application/xml') )
+      .then( json => {
+        const respuesta = { autenticado: false, side: 'LDAP', message: 'Usuario y/o contraseña erronea', response: json};
+        if(json.value){
+          if(json.value.toLowerCase().trim() === 'true'){
+            respuesta.autenticado = true;
+            respuesta.message = 'Autenticado';
+          }
+        }
+        resolve(respuesta)
+      })
+      .catch(err => reject(err) )
+   })
+    .then( jsonResponse => jsonResponse.autenticado ? obtenerDatosByService(userName) : jsonResponse )
+    .catch(err => setMessage('No se encuentra conectado a la red del INEGI') )
+  }
+
+  const obtenerDatosByService = (userName) => {
+    const params = `?cuenta=${userName}`;
+    const peticion = urlPeticion + params;
+    return new Promise( (resolve, reject) => {
+      return fetch(peticion,{
+        method: 'GET',
+        headers : {
+         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8;',
+        }
+       })
+      .then( response => response.status === 404 ? Promise.reject({ message: 'Script no encontrado'}) : response.text())
+      .then( text => text ? JSON.parse(text) : {} )
+      .then( json => {
+        const respuesta = { 
+          autenticado: false, 
+          side: 'Servicio', 
+          message: 'No se encuentra registrado dentro del servicio', 
+          response: json
+        };
+        const keys = Object.keys(json);
+        const propiedad = json[propiedadBajar];
+        if(keys.length > 0 && propiedad){
+          respuesta.autenticado = true;
+          respuesta.message = 'Autenticado desde el servicio';
+          respuesta.response = propiedad;
+        }
+        resolve(respuesta)
+
+      })
+      .catch(err => reject(err) )
+    })
+    return Promise.resolve({userName, peticion})
+  }
+
+  return <>
+      <div>
+        <AppBar />
+        <form className="login">
+          <div className="login-inputs">
+          <div><img src={logoInegi} className="login-logoINEGI" alt="Logo iniegi"></img></div>          
+          
+          <div className="login-inputs-section" >
+            <div  className="login-inputs-txt">Usuario</div>
+            <input  
+                type = "text"
+                spellCheck = "false"
+                className = "login-inputs-inp"
+                onChange={(e)=>setUser(e.target.value)}
+            ></input>
+          </div>          
+          <div className="login-inputs-section" >
+            <div className="login-inputs-txt">Contraseña</div>
+            <input 
+                className="login-inputs-inp"  
+                type="password"
+                onChange={(e)=>setPass(e.target.value)}
+            >
+            </input>
+          </div>
+            <button className="login-inputs-btn" 
+                    type="submit" 
+                    onClick={ validaDatos  }  
+            >Ingresar</button>
+            <div>{message}</div>
+          </div>
+        </form>
+        <div className="login-version"><i className="fa-duotone fa-code"></i> PROTOTIPATOR 1.0</div>
+      </div>
+  </>
+}
+
+export default Login 
+
+
+
+
